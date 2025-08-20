22b28d1509d9942de50c0f57a3ea24feceaeaf13
masterthuku <peterthuku706@gmail.com>
2025-08-21 01:36:01 +0300

    convex

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,8 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "sonner";
 import FloatingShapes from "@/components/floating-shapes";
 import Header from "@/components/header";
+import { ConvexClientProvider } from "./ConvexClientProvider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,22 +17,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className}`}
-      >
-         <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Header/>
-            <main className="bg-slate-900 min-h-screen text-white overflow-x-hidden">
-              <FloatingShapes/>
-              <Toaster richColors/>
-              {children}
-            </main>
-          </ThemeProvider>
+      <body className={`${inter.className}`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ClerkProvider>
+            <ConvexClientProvider>
+              <Header />
+              <main className="bg-slate-900 min-h-screen text-white overflow-x-hidden">
+                <FloatingShapes />
+                <Toaster richColors />
+                {children}
+              </main>
+            </ConvexClientProvider>
+          </ClerkProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/header.jsx
+++ components/header.jsx
@@ -1,8 +1,13 @@
+"use client";
+
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Header = () => {
+  const path = usePathname();
   return (
     <header className="fixed top-6 left-1/2  transform -translate-x-1/2 z-50 text-nowrap">
       <div className="backdrop-blur-md bg-white/10 border border-white/20 rounded-full px-8 py-3 flex items-center justify-between gap-8">
@@ -15,6 +20,41 @@ const Header = () => {
             className="min-w-24 object-cover"
           />
         </Link>
+        {path === "/" && (
+          <div className="hidden md:flex space-x-6">
+            <Link
+              href={"#features"}
+              className="text-white font-medium transition-all duration-300 hover:text-cyan-400 cursor-pointer"
+            >
+              Features
+            </Link>
+            <Link
+              href={"#pricing"}
+              className="text-white font-medium transition-all duration-300 hover:text-cyan-400 cursor-pointer"
+            >
+              Pricing
+            </Link>
+            <Link
+              href={"#contact"}
+              className="text-white font-medium transition-all duration-300 hover:text-cyan-400 cursor-pointer"
+            >
+              Contact
+            </Link>
+          </div>
+        )}
+        <div className="flex items-center gap-3 ml-10 md:ml-20">
+          <SignedOut>
+            <SignInButton />
+            <SignUpButton>
+              <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                Sign Up
+              </button>
+            </SignUpButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
       </div>
     </header>
   );
